956c48fdc44893687fd5def23e69feb819d963ed
Anuar Talipov <anuartb@gmail.com>
2019-05-11 12:16:16 +0900

    initial setup

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    stories = [
+        {
+            'name': 'My Little Pony',
+            'content': 'Once in a while ...'
+        },
+        {
+            'name': 'Kazakh National Fairytale: Aldar Kose',
+            'content': 'In the Kazakh steppes ...'
+        }
+    ]
+    return render_template('index.html', stories=stories)
