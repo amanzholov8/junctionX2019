e8eba4a86975c648be532c35dea3a50837f95332
Anuar Talipov <anuartb@gmail.com>
2019-05-11 21:20:22 +0900

    DeepPavlov example added

---
+++ experimental/deep.py
@@ -0,0 +1,5 @@
+from deeppavlov import build_model, configs
+
+model = build_model(configs.squad.squad, download=False)
+#model.save()
+print(model(['DeepPavlov is library for NLP and dialog systems.'], ['What is DeepPavlov?']))
