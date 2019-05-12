aab23dc5b6c9fdb0ce9d3174110028dec3ab58ab
Anuar Talipov <anuartb@gmail.com>
2019-05-12 09:21:41 +0900

    Fixes again

--- app/static/js/scroll.js
+++ app/static/js/scroll.js
@@ -1,20 +1,32 @@
 var active = false;
 currentPos = 1;
 
+/*
 function scroll() {
     while (active) {
         currentPos += 1;
         $('html, body').animate({
             scrollTop: $(`#div${currentPos}`).offset().top
-        }, 4000);        
+        }, 4000);
         console.log(active);
         delay(400);
     };
 };
+*/
+function scroll() {
+    if(!active){
+        return;
+    }
+    currentPos += 1;
+    $('html, body').animate({
+        scrollTop: $(`#div${currentPos}`).offset().top
+    }, 4000);
+    setTimeout(scroll, 2000);
+}
 
-$(document).on("click", function () {            
+$(document).on("click", function () {
     console.log("received click");
-    debugger;
+    //debugger;
     if (active) {
         active = false;
         console.log(`active after setting to false ${active}`);
